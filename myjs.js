4cc0905007aa556e14da5825a5f1ea81a4153c1b
TommasoGalistu <tommasogalistu92@gmail.com>
2024-06-24 14:06:30 +0200

    fine lavoro + bonus
    aggiunta testo e immagine da data vue

--- myjs.js
+++ myjs.js
@@ -3,8 +3,10 @@ const {createApp} = Vue;
 createApp({
     data(){
         return {
-
+            msg: 'ciao amico',
+            immagineVue: 'https://miro.medium.com/v2/resize:fit:500/1*CPDIH8BWrGipHRJ6o6E2Vw.png'
         }
     },
 
 }).mount('#container');
+
